c00e6b7e63b84d82398eaa1ada93c6a31ed6ef32
AdarDesu <adartruusa@gmail.com>
2022-09-24 05:46:19 +0300

    Week3 tests

---
+++ Test1/Program.cs
@@ -0,0 +1,34 @@
+﻿// If
+
+Console.WriteLine("What grade did you get?");
+
+char Grade = Convert.ToChar(Console.ReadLine().ToUpper());
+
+if (Grade == 'A')
+{
+    Console.WriteLine("Excellent!");
+}
+else if (Grade == 'B')
+{
+    Console.WriteLine("Very good!");
+}
+else if (Grade == 'C')
+{
+    Console.WriteLine("Good.");
+}
+else if (Grade == 'D')
+{
+    Console.WriteLine("Satisfactory.");
+}
+else if (Grade == 'E')
+{
+    Console.WriteLine("Poor.");
+}
+else if (Grade == 'F')
+{
+    Console.WriteLine("Incomplete.");
+}
+else
+{
+    Console.WriteLine("Wrong value.");
+}
---
+++ Test2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Switch
+
+Console.WriteLine("What grade did you get?");
+
+char Grade = Convert.ToChar(Console.ReadLine().ToUpper());
+
+switch (Grade)
+{
+    case 'A':
+        Console.WriteLine("Excellent!");
+        break;
+    case 'B':
+        Console.WriteLine("Very good!");
+        break;
+    case 'C':
+        Console.WriteLine("Good.");
+        break;
+    case 'D':
+        Console.WriteLine("Satisfactory.");
+        break;
+    case 'E':
+        Console.WriteLine("Poor.");
+        break;
+    case 'F':
+        Console.WriteLine("Incomplete.");
+        break;
+    default:
+        Console.WriteLine("Wrong value.");
+        break;
+}
